38849c4364bf124895de1bffb16bb4f06caa9623
craigedneyai <craig@craigedney.ai>
2021-11-19 19:28:44 +0300

    fix styling on BackButton.js

--- components/BackButton.js
+++ components/BackButton.js
@@ -6,7 +6,7 @@ export default function BackButton() {
 
   return (
     <button
-      className="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium text-gray-700 hover:text-gray-900"
+      className="inline-flex items-center px-3 py-2 text-sm leading-4 font-medium text-gray-700 hover:text-gray-900"
       type="button"
       onClick={() => router.back()}
     >
