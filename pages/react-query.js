80ddc7b73df1d3c20e8baa73bbcce63ca8d5ea2c
craigedneyai <craig@craigedney.ai>
2021-11-19 19:26:43 +0300

    Added layout styling and backbutton to react-query page

--- pages/react-query.js
+++ pages/react-query.js
@@ -1,12 +1,15 @@
 import DescriptionList from "../components/DescriptionList";
+import Layout from "../components/Layout";
+import BackButton from "../components/BackButton";
 
 export default function ReactQueryPage() {
   return (
-    <>
+    <Layout>
+      <BackButton />
       <DescriptionList
         title="React Query Example"
         description="In this example..."
       />
-    </>
+    </Layout>
   );
 }
